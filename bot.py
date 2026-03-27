b15b51d64398047c002974550199d5d6ce17f53d
vkdg0410 <85492736+vkdg0410@users.noreply.github.com>
2026-03-27 20:34:48 +0100

    Change environment variable from 'TOKEN' to 'DISCORD_TOKEN'

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from discord import app_commands
 from discord.ext import commands
 
-TOKEN = os.environ.get('TOKEN')
+TOKEN = os.environ.get('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
